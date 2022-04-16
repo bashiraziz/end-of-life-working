66694cbb71b80b9bd89c28f82372959172664098
bashiraziz <bashiraziz@gmail.com>
2022-04-16 13:33:33 -0400

    firebase.js added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import db from "./firebase";
 
 // styles
 import './App.css'
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+ apiKey: "AIzaSyDW8mvWzN-GKwvKDkB3mgZm1ER6ReUMqec",
+ authDomain: "end-of-journey-c0bc7.firebaseapp.com",
+ projectId: "end-of-journey-c0bc7",
+ storageBucket: "end-of-journey-c0bc7.appspot.com",
+ messagingSenderId: "124527430277",
+ appId: "1:124527430277:web:b0d980fed3171ed1b477ec",
+ measurementId: "G-5MPND5M3RH"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+  
+export default db;
